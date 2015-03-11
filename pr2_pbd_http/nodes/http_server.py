f37fcf455c811b359724899809ccd5a42a18ff1f
Sonya Alexandrova <sonyaa@cs.washington.edu>
2015-03-11 10:16:09 -0700

    moving www files to pr2_pbd_interaction

--- pr2_pbd_http/nodes/http_server.py
+++ pr2_pbd_http/nodes/http_server.py
@@ -13,7 +13,7 @@ def server():
 
     rospack = rospkg.RosPack()
 
-    os.chdir(os.path.join(rospack.get_path('pr2_pbd_http'), "html"))
+    os.chdir(os.path.join(rospack.get_path('pr2_pbd_interaction'), "www"))
 
     HandlerClass = SimpleHTTPRequestHandler
     ServerClass = BaseHTTPServer.HTTPServer
