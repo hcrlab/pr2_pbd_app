bc16b5fd42ebf11e937e65ac901a143b7f25d25a
Sonya Alexandrova <sonyaa@cs.washington.edu>
2015-05-11 10:25:26 -0700

    get rid of dependencies - copy function for getting head pose

--- pr2_pbd_interaction/nodes/save_head_pose.py
+++ pr2_pbd_interaction/nodes/save_head_pose.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python
+from geometry_msgs.msg import PointStamped, Point
 import roslib
+from tf import TransformListener
+import tf
+
 roslib.load_manifest('pr2_pbd_interaction')
 
 from pr2_pbd_interaction.Robot import Robot
@@ -10,6 +14,24 @@ import yaml
 
 import rospy
 
+
+def get_head_position():
+    try:
+        tf_listener = TransformListener()
+        ref_frame = "/head_tilt_link"
+        timestamp = tf_listener.getLatestCommonTime(ref_frame,
+                                                          "/base_link")
+        point_stamped = PointStamped()
+        point_stamped.header.frame_id = ref_frame
+        point_stamped.header.stamp = timestamp
+        point_stamped.point = Point(1, 0, 0)
+        head_position = tf_listener.transformPoint("/base_link", point_stamped).point
+        return head_position
+    except (tf.LookupException, tf.ConnectivityException,
+            tf.ExtrapolationException) as e:
+        rospy.logwarn('Something wrong with transform request for head state.' + str(e))
+        return None
+
 if __name__ == '__main__':
     rospy.init_node('save_head_pose')
     data_directory = rospy.get_param('/pr2_pbd_interaction/headPosesRoot', '.')
@@ -17,7 +39,7 @@ if __name__ == '__main__':
     if not os.path.exists(data_directory):
         os.makedirs(data_directory)
     pose_msg = HeadPoseData()
-    pose_msg.head_pose = Robot.get_head_position()
+    pose_msg.head_pose = get_head_position()
     pose_msg.id = 0
     while os.path.isfile(data_directory + str(pose_msg.id) + file_extension):
         pose_msg.id += 1
