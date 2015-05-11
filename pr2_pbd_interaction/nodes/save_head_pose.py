0764a5a8e0fd592f6a73ad9f09d6e01341650f37
Sonya Alexandrova <sonyaa@cs.washington.edu>
2015-05-11 10:11:28 -0700

    change parameters for head pose saving node

--- pr2_pbd_interaction/nodes/save_head_pose.py
+++ pr2_pbd_interaction/nodes/save_head_pose.py
@@ -12,7 +12,7 @@ import rospy
 
 if __name__ == '__main__':
     rospy.init_node('save_head_pose')
-    data_directory = rospy.get_param('/pr2_pbd_interaction/headPosesRoot')
+    data_directory = rospy.get_param('/pr2_pbd_interaction/headPosesRoot', '.')
     file_extension = rospy.get_param('/pr2_pbd_interaction/fileExtension', '.yaml')
     if not os.path.exists(data_directory):
         os.makedirs(data_directory)
@@ -21,7 +21,7 @@ if __name__ == '__main__':
     pose_msg.id = 0
     while os.path.isfile(data_directory + str(pose_msg.id) + file_extension):
         pose_msg.id += 1
-    pose_msg.name = sys.argv[0] if len(sys.argv) > 0 else 'Head pose ' + str(pose_msg.id)
+    pose_msg.name = sys.argv[1] if len(sys.argv) > 1 else 'Head pose ' + str(pose_msg.id)
     pose_file = open(data_directory + str(pose_msg.id) + file_extension, 'w')
     pose_file.write(yaml.dump(pose_msg))
     pose_file.close()
