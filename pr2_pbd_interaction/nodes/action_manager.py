9e815383670916234ebf3f465ba1d49c2706a836
Sonya Alexandrova <sonyaa@cs.washington.edu>
2015-05-10 22:55:09 -0700

    added service to get saved head poses + node to save head pose (NOT TESTED + this should probably be in a separate package)

--- pr2_pbd_interaction/nodes/action_manager.py
+++ pr2_pbd_interaction/nodes/action_manager.py
@@ -3,11 +3,25 @@ import roslib
 roslib.load_manifest('pr2_pbd_interaction')
 
 from pr2_pbd_interaction.step_types import ManipulationStep
-from pr2_pbd_interaction.msg import ActionData
-from pr2_pbd_interaction.srv import GetSavedActions, GetSavedActionsResponse
+from pr2_pbd_interaction.msg import ActionData, HeadPoseData
+from pr2_pbd_interaction.srv import GetSavedActions, GetSavedActionsResponse, \
+    GetSavedHeadPoses, GetSavedHeadPosesResponse
 
 
 import rospy
+
+def get_saved_head_poses(dummy):
+    head_poses = []
+    data_directory = rospy.get_param('/pr2_pbd_interaction/headPosesRoot')
+    file_extension = rospy.get_param('/pr2_pbd_interaction/fileExtension', '.yaml')
+    if not os.path.exists(data_directory):
+        return []
+    for f in listdir(data_directory):
+        file_path = join(data_directory, f)
+        if isfile(file_path) and file_path.endswith(file_extension):
+            with open(file_path, 'r') as content_file:
+                head_poses.append()
+    return GetSavedHeadPosesResponse(head_poses)
 
 
 def get_saved_actions(dummy):
@@ -21,4 +35,5 @@ def get_saved_actions(dummy):
 if __name__ == '__main__':
     rospy.init_node('action_manager')
     s1 = rospy.Service('get_saved_actions', GetSavedActions, get_saved_actions)
+    s2 = rospy.Service('get_saved_head_poses', GetSavedHeadPoses, get_saved_head_poses)
     rospy.spin()
---
+++ pr2_pbd_interaction/nodes/save_head_pose.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+import roslib
+roslib.load_manifest('pr2_pbd_interaction')
+
+from pr2_pbd_interaction.Robot import Robot
+from pr2_pbd_interaction.msg import HeadPoseData
+import sys
+import os
+from os import listdir
+from os.path import isfile, exists
+
+import rospy
+
+if __name__ == '__main__':
+    rospy.init_node('save_head_pose')
+    data_directory = rospy.get_param('/pr2_pbd_interaction/headPosesRoot')
+    file_extension = rospy.get_param('/pr2_pbd_interaction/fileExtension', '.yaml')
+    if not os.path.exists(data_directory):
+        os.makedirs(data_directory)
+    pose_msg = HeadPoseData()
+    pose_msg.head_pose = Robot.get_robot().get_head_position()
+    pose_msg.id = 0
+    while os.path.isfile(data_directory + str(pose_msg.id) + file_extension):
+        pose_msg.id += 1
+    pose_msg.name = sys.argv[0] if len(sys.argv) > 0 else 'Head pose ' + str(pose_msg.id)
+    pose_file = open(data_directory + str(pose_msg.id) + file_extension, 'w')
+    pose_file.write(yaml.dump(pose_msg))
+    pose_file.close()
+
